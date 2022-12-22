c5320f217a0caf507df692fb5626b7e95c0e198e
mostafaashraf204 <mostafaashraf3100@gmaill.com>
2022-12-22 11:23:48 +0200

    finish site

--- js/main.js
+++ js/main.js
@@ -18,3 +18,19 @@ counters.forEach(counter => {
 
     animate();
 });
+
+
+
+$(function() {
+    var header = $(".navbar");
+
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+        if (scroll >= 50) {
+            header.addClass("scrolled");
+        } else {
+            header.removeClass("scrolled");
+        }
+    });
+
+});
